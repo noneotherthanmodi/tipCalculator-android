47af0edc9b2040a92de05698813eacaa67328b42
Udit <uditmodi03@gmail.com>
2023-11-28 02:10:12 +0530

    tip calculator

--- app/src/main/java/com/example/tipcalculatorandroid/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculatorandroid/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -29,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -90,7 +92,11 @@ fun EditNumberField(modifier: Modifier = Modifier){
     var amountInput by remember { mutableStateOf("") }
     TextField(value = amountInput,
         onValueChange = { amountInput = it },
-        modifier = Modifier)
+        label = { Text(text = stringResource(id =R.string.bill_amount))},
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier,
+    )
 }
 
 
